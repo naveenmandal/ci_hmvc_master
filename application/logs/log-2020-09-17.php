af9404cf0b2ad9976a6493abb31cdcc5ccd7fd22
Naveen Mandal <naveen.mandal1@gmail.com>
2020-09-17 09:37:49 +0530

    Initial Commit

---
+++ application/logs/log-2020-09-17.php
@@ -0,0 +1,220 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2020-09-17 06:03:17 --> Config Class Initialized
+INFO - 2020-09-17 06:03:17 --> Hooks Class Initialized
+DEBUG - 2020-09-17 06:03:17 --> UTF-8 Support Enabled
+INFO - 2020-09-17 06:03:17 --> Utf8 Class Initialized
+INFO - 2020-09-17 06:03:17 --> URI Class Initialized
+DEBUG - 2020-09-17 06:03:17 --> No URI present. Default controller set.
+INFO - 2020-09-17 06:03:17 --> Router Class Initialized
+INFO - 2020-09-17 06:03:17 --> Output Class Initialized
+INFO - 2020-09-17 06:03:17 --> Security Class Initialized
+DEBUG - 2020-09-17 06:03:17 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-09-17 06:03:17 --> Input Class Initialized
+INFO - 2020-09-17 06:03:17 --> Language Class Initialized
+INFO - 2020-09-17 06:03:17 --> Language Class Initialized
+INFO - 2020-09-17 06:03:17 --> Config Class Initialized
+INFO - 2020-09-17 06:03:17 --> Loader Class Initialized
+ERROR - 2020-09-17 06:03:17 --> Severity: 8192 --> Unparenthesized `a ? b : c ? d : e` is deprecated. Use either `(a ? b : c) ? d : e` or `a ? b : (c ? d : e)` C:\xampp\htdocs\ci_hmvc\system\helpers\url_helper.php 162
+INFO - 2020-09-17 06:03:17 --> Helper loaded: url_helper
+INFO - 2020-09-17 06:03:17 --> Helper loaded: file_helper
+INFO - 2020-09-17 06:03:17 --> Database Driver Class Initialized
+ERROR - 2020-09-17 06:03:17 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\ci_hmvc\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2020-09-17 06:03:17 --> Unable to connect to the database
+INFO - 2020-09-17 06:03:17 --> Language file loaded: language/english/db_lang.php
+INFO - 2020-09-17 06:03:54 --> Config Class Initialized
+INFO - 2020-09-17 06:03:54 --> Hooks Class Initialized
+DEBUG - 2020-09-17 06:03:54 --> UTF-8 Support Enabled
+INFO - 2020-09-17 06:03:54 --> Utf8 Class Initialized
+INFO - 2020-09-17 06:03:54 --> URI Class Initialized
+DEBUG - 2020-09-17 06:03:54 --> No URI present. Default controller set.
+INFO - 2020-09-17 06:03:54 --> Router Class Initialized
+INFO - 2020-09-17 06:03:54 --> Output Class Initialized
+INFO - 2020-09-17 06:03:54 --> Security Class Initialized
+DEBUG - 2020-09-17 06:03:54 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-09-17 06:03:54 --> Input Class Initialized
+INFO - 2020-09-17 06:03:54 --> Language Class Initialized
+INFO - 2020-09-17 06:03:54 --> Language Class Initialized
+INFO - 2020-09-17 06:03:54 --> Config Class Initialized
+INFO - 2020-09-17 06:03:54 --> Loader Class Initialized
+INFO - 2020-09-17 06:03:54 --> Database Driver Class Initialized
+ERROR - 2020-09-17 06:03:54 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\ci_hmvc\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2020-09-17 06:03:54 --> Unable to connect to the database
+INFO - 2020-09-17 06:03:54 --> Language file loaded: language/english/db_lang.php
+INFO - 2020-09-17 06:04:23 --> Config Class Initialized
+INFO - 2020-09-17 06:04:23 --> Hooks Class Initialized
+DEBUG - 2020-09-17 06:04:23 --> UTF-8 Support Enabled
+INFO - 2020-09-17 06:04:23 --> Utf8 Class Initialized
+INFO - 2020-09-17 06:04:23 --> URI Class Initialized
+DEBUG - 2020-09-17 06:04:23 --> No URI present. Default controller set.
+INFO - 2020-09-17 06:04:23 --> Router Class Initialized
+INFO - 2020-09-17 06:04:23 --> Output Class Initialized
+INFO - 2020-09-17 06:04:23 --> Security Class Initialized
+DEBUG - 2020-09-17 06:04:23 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-09-17 06:04:23 --> Input Class Initialized
+INFO - 2020-09-17 06:04:23 --> Language Class Initialized
+INFO - 2020-09-17 06:04:23 --> Language Class Initialized
+INFO - 2020-09-17 06:04:23 --> Config Class Initialized
+INFO - 2020-09-17 06:04:23 --> Loader Class Initialized
+INFO - 2020-09-17 06:04:23 --> Controller Class Initialized
+DEBUG - 2020-09-17 06:04:23 --> Welcome MX_Controller Initialized
+DEBUG - 2020-09-17 06:04:23 --> File loaded: C:\xampp\htdocs\ci_hmvc\application\modules/Welcome/views/welcome_message.php
+INFO - 2020-09-17 06:04:23 --> Final output sent to browser
+DEBUG - 2020-09-17 06:04:23 --> Total execution time: 0.1318
+INFO - 2020-09-17 06:04:30 --> Config Class Initialized
+INFO - 2020-09-17 06:04:30 --> Hooks Class Initialized
+DEBUG - 2020-09-17 06:04:30 --> UTF-8 Support Enabled
+INFO - 2020-09-17 06:04:30 --> Utf8 Class Initialized
+INFO - 2020-09-17 06:04:30 --> URI Class Initialized
+DEBUG - 2020-09-17 06:04:30 --> No URI present. Default controller set.
+INFO - 2020-09-17 06:04:30 --> Router Class Initialized
+INFO - 2020-09-17 06:04:30 --> Output Class Initialized
+INFO - 2020-09-17 06:04:30 --> Security Class Initialized
+DEBUG - 2020-09-17 06:04:30 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-09-17 06:04:30 --> Input Class Initialized
+INFO - 2020-09-17 06:04:30 --> Language Class Initialized
+INFO - 2020-09-17 06:04:30 --> Language Class Initialized
+INFO - 2020-09-17 06:04:30 --> Config Class Initialized
+INFO - 2020-09-17 06:04:30 --> Loader Class Initialized
+INFO - 2020-09-17 06:04:30 --> Controller Class Initialized
+DEBUG - 2020-09-17 06:04:30 --> Welcome MX_Controller Initialized
+DEBUG - 2020-09-17 06:04:30 --> File loaded: C:\xampp\htdocs\ci_hmvc\application\modules/Welcome/views/welcome_message.php
+INFO - 2020-09-17 06:04:30 --> Final output sent to browser
+DEBUG - 2020-09-17 06:04:30 --> Total execution time: 0.1335
+INFO - 2020-09-17 06:05:04 --> Config Class Initialized
+INFO - 2020-09-17 06:05:04 --> Hooks Class Initialized
+DEBUG - 2020-09-17 06:05:04 --> UTF-8 Support Enabled
+INFO - 2020-09-17 06:05:04 --> Utf8 Class Initialized
+INFO - 2020-09-17 06:05:04 --> URI Class Initialized
+DEBUG - 2020-09-17 06:05:04 --> No URI present. Default controller set.
+INFO - 2020-09-17 06:05:04 --> Router Class Initialized
+INFO - 2020-09-17 06:05:04 --> Output Class Initialized
+INFO - 2020-09-17 06:05:04 --> Security Class Initialized
+DEBUG - 2020-09-17 06:05:04 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-09-17 06:05:04 --> Input Class Initialized
+INFO - 2020-09-17 06:05:04 --> Language Class Initialized
+INFO - 2020-09-17 06:05:04 --> Language Class Initialized
+INFO - 2020-09-17 06:05:04 --> Config Class Initialized
+INFO - 2020-09-17 06:05:04 --> Loader Class Initialized
+INFO - 2020-09-17 06:05:04 --> Controller Class Initialized
+DEBUG - 2020-09-17 06:05:04 --> Welcome MX_Controller Initialized
+DEBUG - 2020-09-17 06:05:04 --> File loaded: C:\xampp\htdocs\ci_hmvc\application\modules/Welcome/views/welcome_message.php
+INFO - 2020-09-17 06:05:04 --> Final output sent to browser
+DEBUG - 2020-09-17 06:05:04 --> Total execution time: 0.1352
+INFO - 2020-09-17 06:05:11 --> Config Class Initialized
+INFO - 2020-09-17 06:05:11 --> Hooks Class Initialized
+DEBUG - 2020-09-17 06:05:11 --> UTF-8 Support Enabled
+INFO - 2020-09-17 06:05:11 --> Utf8 Class Initialized
+INFO - 2020-09-17 06:05:11 --> URI Class Initialized
+DEBUG - 2020-09-17 06:05:11 --> No URI present. Default controller set.
+INFO - 2020-09-17 06:05:11 --> Router Class Initialized
+INFO - 2020-09-17 06:05:11 --> Output Class Initialized
+INFO - 2020-09-17 06:05:11 --> Security Class Initialized
+DEBUG - 2020-09-17 06:05:11 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-09-17 06:05:11 --> Input Class Initialized
+INFO - 2020-09-17 06:05:11 --> Language Class Initialized
+INFO - 2020-09-17 06:05:11 --> Language Class Initialized
+INFO - 2020-09-17 06:05:11 --> Config Class Initialized
+INFO - 2020-09-17 06:05:11 --> Loader Class Initialized
+INFO - 2020-09-17 06:05:11 --> Controller Class Initialized
+DEBUG - 2020-09-17 06:05:11 --> Welcome MX_Controller Initialized
+DEBUG - 2020-09-17 06:05:11 --> File loaded: C:\xampp\htdocs\ci_hmvc\application\modules/Welcome/views/welcome_message.php
+INFO - 2020-09-17 06:05:11 --> Final output sent to browser
+DEBUG - 2020-09-17 06:05:11 --> Total execution time: 0.1299
+INFO - 2020-09-17 06:05:29 --> Config Class Initialized
+INFO - 2020-09-17 06:05:29 --> Hooks Class Initialized
+DEBUG - 2020-09-17 06:05:29 --> UTF-8 Support Enabled
+INFO - 2020-09-17 06:05:29 --> Utf8 Class Initialized
+INFO - 2020-09-17 06:05:29 --> URI Class Initialized
+DEBUG - 2020-09-17 06:05:29 --> No URI present. Default controller set.
+INFO - 2020-09-17 06:05:29 --> Router Class Initialized
+INFO - 2020-09-17 06:05:29 --> Output Class Initialized
+INFO - 2020-09-17 06:05:29 --> Security Class Initialized
+DEBUG - 2020-09-17 06:05:29 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-09-17 06:05:29 --> Input Class Initialized
+INFO - 2020-09-17 06:05:29 --> Language Class Initialized
+INFO - 2020-09-17 06:05:29 --> Language Class Initialized
+INFO - 2020-09-17 06:05:29 --> Config Class Initialized
+INFO - 2020-09-17 06:05:29 --> Loader Class Initialized
+INFO - 2020-09-17 06:05:29 --> Controller Class Initialized
+DEBUG - 2020-09-17 06:05:29 --> Welcome MX_Controller Initialized
+DEBUG - 2020-09-17 06:05:29 --> File loaded: C:\xampp\htdocs\ci_hmvc\application\modules/Welcome/views/welcome_message.php
+INFO - 2020-09-17 06:05:29 --> Final output sent to browser
+DEBUG - 2020-09-17 06:05:29 --> Total execution time: 0.1415
+INFO - 2020-09-17 06:05:32 --> Config Class Initialized
+INFO - 2020-09-17 06:05:32 --> Hooks Class Initialized
+DEBUG - 2020-09-17 06:05:32 --> UTF-8 Support Enabled
+INFO - 2020-09-17 06:05:32 --> Utf8 Class Initialized
+INFO - 2020-09-17 06:05:32 --> URI Class Initialized
+INFO - 2020-09-17 06:05:32 --> Router Class Initialized
+INFO - 2020-09-17 06:05:32 --> Output Class Initialized
+INFO - 2020-09-17 06:05:32 --> Security Class Initialized
+DEBUG - 2020-09-17 06:05:32 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-09-17 06:05:32 --> Input Class Initialized
+INFO - 2020-09-17 06:05:32 --> Language Class Initialized
+INFO - 2020-09-17 06:05:32 --> Language Class Initialized
+INFO - 2020-09-17 06:05:32 --> Config Class Initialized
+INFO - 2020-09-17 06:05:32 --> Loader Class Initialized
+INFO - 2020-09-17 06:05:32 --> Controller Class Initialized
+DEBUG - 2020-09-17 06:05:32 --> Welcome MX_Controller Initialized
+DEBUG - 2020-09-17 06:05:32 --> File loaded: C:\xampp\htdocs\ci_hmvc\application\modules/Welcome/views/welcome_message.php
+INFO - 2020-09-17 06:05:32 --> Final output sent to browser
+DEBUG - 2020-09-17 06:05:32 --> Total execution time: 0.1248
+INFO - 2020-09-17 06:05:34 --> Config Class Initialized
+INFO - 2020-09-17 06:05:34 --> Hooks Class Initialized
+DEBUG - 2020-09-17 06:05:34 --> UTF-8 Support Enabled
+INFO - 2020-09-17 06:05:34 --> Utf8 Class Initialized
+INFO - 2020-09-17 06:05:34 --> URI Class Initialized
+INFO - 2020-09-17 06:05:34 --> Router Class Initialized
+INFO - 2020-09-17 06:05:34 --> Output Class Initialized
+INFO - 2020-09-17 06:05:34 --> Security Class Initialized
+DEBUG - 2020-09-17 06:05:34 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-09-17 06:05:34 --> Input Class Initialized
+INFO - 2020-09-17 06:05:34 --> Language Class Initialized
+INFO - 2020-09-17 06:05:34 --> Language Class Initialized
+INFO - 2020-09-17 06:05:34 --> Config Class Initialized
+INFO - 2020-09-17 06:05:34 --> Loader Class Initialized
+INFO - 2020-09-17 06:05:34 --> Controller Class Initialized
+DEBUG - 2020-09-17 06:05:34 --> Welcome MX_Controller Initialized
+DEBUG - 2020-09-17 06:05:34 --> File loaded: C:\xampp\htdocs\ci_hmvc\application\modules/Welcome/views/welcome_message.php
+INFO - 2020-09-17 06:05:34 --> Final output sent to browser
+DEBUG - 2020-09-17 06:05:34 --> Total execution time: 0.1266
+INFO - 2020-09-17 06:05:35 --> Config Class Initialized
+INFO - 2020-09-17 06:05:35 --> Hooks Class Initialized
+DEBUG - 2020-09-17 06:05:35 --> UTF-8 Support Enabled
+INFO - 2020-09-17 06:05:35 --> Utf8 Class Initialized
+INFO - 2020-09-17 06:05:35 --> URI Class Initialized
+INFO - 2020-09-17 06:05:35 --> Router Class Initialized
+INFO - 2020-09-17 06:05:35 --> Output Class Initialized
+INFO - 2020-09-17 06:05:35 --> Security Class Initialized
+DEBUG - 2020-09-17 06:05:35 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-09-17 06:05:35 --> Input Class Initialized
+INFO - 2020-09-17 06:05:35 --> Language Class Initialized
+INFO - 2020-09-17 06:05:35 --> Language Class Initialized
+INFO - 2020-09-17 06:05:35 --> Config Class Initialized
+INFO - 2020-09-17 06:05:35 --> Loader Class Initialized
+INFO - 2020-09-17 06:05:35 --> Controller Class Initialized
+DEBUG - 2020-09-17 06:05:35 --> Welcome MX_Controller Initialized
+DEBUG - 2020-09-17 06:05:35 --> File loaded: C:\xampp\htdocs\ci_hmvc\application\modules/Welcome/views/welcome_message.php
+INFO - 2020-09-17 06:05:35 --> Final output sent to browser
+DEBUG - 2020-09-17 06:05:35 --> Total execution time: 0.1258
+INFO - 2020-09-17 06:05:38 --> Config Class Initialized
+INFO - 2020-09-17 06:05:38 --> Hooks Class Initialized
+DEBUG - 2020-09-17 06:05:38 --> UTF-8 Support Enabled
+INFO - 2020-09-17 06:05:38 --> Utf8 Class Initialized
+INFO - 2020-09-17 06:05:38 --> URI Class Initialized
+DEBUG - 2020-09-17 06:05:38 --> No URI present. Default controller set.
+INFO - 2020-09-17 06:05:38 --> Router Class Initialized
+INFO - 2020-09-17 06:05:38 --> Output Class Initialized
+INFO - 2020-09-17 06:05:38 --> Security Class Initialized
+DEBUG - 2020-09-17 06:05:38 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-09-17 06:05:38 --> Input Class Initialized
+INFO - 2020-09-17 06:05:38 --> Language Class Initialized
+INFO - 2020-09-17 06:05:38 --> Language Class Initialized
+INFO - 2020-09-17 06:05:38 --> Config Class Initialized
+INFO - 2020-09-17 06:05:38 --> Loader Class Initialized
+INFO - 2020-09-17 06:05:38 --> Controller Class Initialized
+DEBUG - 2020-09-17 06:05:38 --> Welcome MX_Controller Initialized
+DEBUG - 2020-09-17 06:05:38 --> File loaded: C:\xampp\htdocs\ci_hmvc\application\modules/Welcome/views/welcome_message.php
+INFO - 2020-09-17 06:05:38 --> Final output sent to browser
+DEBUG - 2020-09-17 06:05:38 --> Total execution time: 0.1361
